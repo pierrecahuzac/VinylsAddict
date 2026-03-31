df01ed7f379b67e444f60a95385ed557f5ad5dde
Thaliios <cahuzac.p@gmail.com>
2026-03-31 22:08:48 +0200

    implement search bar

---
+++ src/components/hearder/index.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "../searchBar";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <title>Header</title>
+      <SearchBar></SearchBar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/pages/collection/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../../hearder";
+
+const Collection = () => {
+  return (
+    <div>
+      <Header />
+     <h1>Ma collection</h1> 
+    </div>
+  );
+};
+
+export default Collection;
