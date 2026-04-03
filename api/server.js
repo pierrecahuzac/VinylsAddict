952d67d95f22e72c2852900b27edfc2846192978
Thaliios <cahuzac.p@gmail.com>
2026-04-04 00:32:17 +0200

    models
    feat: implement prismaClient and seed functionality with seedData.json
    
    fix: update server.js to include a welcome route and adjust user creation logic
    
    fix: update docker-compose.yml to reference the correct .env file path

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import pkg from '@prisma/client';
+import express from "express";
+import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 
@@ -8,23 +8,25 @@ const prisma = new PrismaClient();
 const port = process.env.PORT || 33000;
 
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur l'API de Vinyls Addict !");
+});
 // Route de test pour créer un utilisateur et voir si la DB répond
-app.post('/setup-test', async (req, res) => {
+app.post("/setup-test", async (req, res) => {
   try {
     const newUser = await prisma.user.create({
       data: {
-        email: 'test@vinyls.com',
-        name: 'Thaliios',
+        email: "test@vinyls.com",
+        name: "Thaliios",
       },
     });
-    res.json({ message: 'User créé !', user: newUser });
+    res.json({ message: "User créé !", user: newUser });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
@@ -32,3 +34,5 @@ app.get('/users', async (req, res) => {
 app.listen(port, () => {
   console.log(`API lancée sur le port ${port}`);
 });
+
+
